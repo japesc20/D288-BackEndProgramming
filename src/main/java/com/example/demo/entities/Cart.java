1e8bc3c26c700c4692a910ad423029cbc59861ea
Jake <jpetrak@wgu.edu>
2024-10-14 17:31:24 -0700

    Created Entity classes, finished Cart class. (10/14 -- Part D)

---
+++ src/main/java/com/example/demo/entities/Cart.java
@@ -0,0 +1,46 @@
+package com.example.demo.entities;
+
+import com.fasterxml.jackson.databind.DatabindException;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table (name = "carts")
+@Data
+
+public class Cart {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "cart_id")
+    private Long cart_id;
+
+    @Column (name = "package_price")
+    private BigDecimal package_price;
+
+    @Column (name = "party_size")
+    private int party_size;
+
+    @Enumerated(EnumType.STRING)
+    @Column (name = "status")
+    private StatusType status;
+
+    @Column (name = "order_tracking_number")
+    private String order_tracking_number;
+
+    @Column (name = "create_date")
+    @CreationTimestamp
+    private Date create_date;
+
+    @Column (name = "last_update")
+    @UpdateTimestamp
+    private Date last_update;
+
+    @Column (name = "customer_id")
+    private Long customer_id;
+
+}
